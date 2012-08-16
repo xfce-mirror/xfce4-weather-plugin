86fa99f36b3fa554c9cf0d4325f900e46756903b
Harald Judt <h.judt@gmx.at>
2012-08-16 15:41:42 +0200

    Fix last commit.
    Automagic fails sometimes...

--- panel-plugin/weather-http.c
+++ panel-plugin/weather-http.c
@@ -1,4 +1,4 @@
-/*  Copyright (c) 2003-2012 Xfce Development Team
+/* Copyright (c) 2003-2012 Xfce Development Team
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,9 +14,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
- */
-e
- * Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #ifdef HAVE_CONFIG_H
