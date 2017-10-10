7c976663e8790e5b06ca31a4a3165b11aeb89f11
Szymon Siwek <slsiwek+xfce@gmail.com>
2017-10-10 17:16:35 +0200

    Set USER_AGENT for nominatim search (bug #13877)
    This is required by https://operations.osmfoundation.org/policies/nominatim/.
    Apparently, this is a new requirement or one that has not been enforced until
    recently.

--- panel-plugin/weather.c
+++ panel-plugin/weather.c
@@ -1849,6 +1849,8 @@ xfceweather_create_control(XfcePanelPlugin *plugin)
 
     /* Setup session for HTTP connections */
     data->session = soup_session_async_new();
+    g_object_set(data->session, SOUP_SESSION_USER_AGENT,
+                 PACKAGE_NAME "-" PACKAGE_VERSION, NULL);
     g_object_set(data->session, SOUP_SESSION_TIMEOUT,
                  CONN_TIMEOUT, NULL);
 
