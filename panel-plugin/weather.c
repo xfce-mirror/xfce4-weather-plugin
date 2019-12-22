abd77251273d0cfaac29c883217dcfdb5b257830
Olivier Duchateau <duchateau.olivier@gmail.com>
2019-12-22 19:29:58 +0100

    Switch to 'locationforecast' product and use a more recent API version (bug #16268)

--- panel-plugin/weather.c
+++ panel-plugin/weather.c
@@ -588,6 +588,7 @@ cb_weather_update(SoupSession *session,
 static gboolean
 update_handler(plugin_data *data)
 {
+    gchar *api_version = FORECAST_API;
     gchar *url;
     gboolean night_time;
     time_t now_t;
@@ -653,9 +654,10 @@ update_handler(plugin_data *data)
         data->weather_update->started = TRUE;
 
         /* build url */
-        url = g_strdup_printf("https://api.met.no/weatherapi"
-                              "/locationforecastlts/1.3/?lat=%s&lon=%s&"
-                              "msl=%d",
+        url = g_strdup_printf("https://api.met.no"
+                              "/weatherapi/locationforecast/%s/"
+                              "?lat=%s&lon=%s&msl=%d",
+                              api_version,
                               data->lat, data->lon, data->msl);
 
         /* start receive thread */
